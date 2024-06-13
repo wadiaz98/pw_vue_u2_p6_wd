b41a8a5ed268125fff3066adfe845836fb510688
wadias98 <wadiaz@uce.edu.ec>
2024-06-12 19:50:07 -0500

    taller19

--- src/components/Pregunta.vue
+++ src/components/Pregunta.vue
@@ -1,23 +1,92 @@
 <template>
-    <img src="https://yesno.wtf/assets/yes/14-b57c6dc03aa15a4b18f53eb50d6197ee.gif" alt="Not found">
-    <div-oscuro>
-        
-    </div-oscuro>
-    
-    <div class="pregunta-container">
-        <input type="text" placeholder="Hazme una pregunta ">
-        <p>Recuerda terminar las pregunta con (?)</p>
-        <div>
-            <h2>Voy a pasar de semestre</h2>
-            <h1> Si, No,.....</h1>
-        </div>
+  <img v-if="image" :src="image" alt="Not found" />
+
+  <div class="oscuro"></div>
+  <div class="pregunta_container">
+    <input v-model="pregunta" type="text" placeholder="Hazme una pregunta " />
+    <p>Recuerda terminar las pregunta con (?)</p>
+    <div class="respuesta">
+      <h2>{{ pregunta }}</h2>
+      <h1>{{ respuesta }}</h1>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      respuesta:null,
+      pregunta: null,
+      image:null,
+    };
+  },
+  watch: {
+    pregunta(value, oldValue) {
+      console.log({ value, oldValue });
+      if (!value.includes("?")) {
+        this.respuesta = null;
+        return; /* Salgase del observador */
+      }
+      //Consumir el API para obtener la respuesta
+      this.getResponse();
+    },
+  },
+  methods: {
+    async getResponse() {
+      this.respuesta = "Pensando......"
+      const data = await fetch("https://yesno.wtf/api").then((r) => r.json());
+      this.respuesta = data.answer;
+      this.image = data.image;
+    },
+  },
+};
 </script>
 
-<style></style>
+<style>
+img,
+.oscuro {
+  max-width: 100%;
+  height: 100vh;
+  max-height: 100%;
+  width: 100vw;
+  position: fixed; /* Irrespeta la posición poniendole sobre los demas */
+  top: 0px;
+  left: 0px;
+}
+
+.oscuro {
+  background-color: rgba(0, 0, 0, 0.5);
+  /* 0.0 full transparente y 1.0 full opaco */
+}
+
+.pregunta_container {
+  position: relative;
+}
+
+p,
+h1,
+h2 {
+  color: white;
+}
+input {
+  margin-top: 70px;
+  width: 260px;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+}
+
+input:focus {
+  outline: none;
+}
+
+.respuesta {
+  margin-top: 100px;
+}
+
+P {
+  font-size: 25px;
+  margin-top: 0px;
+}
+</style>
